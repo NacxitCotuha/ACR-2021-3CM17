7f8831bb18547ebc5bad7fa591acf7258791746e
NacxitQuetzalcoatl <86132894+NacxitCotuha@users.noreply.github.com>
2021-09-02 12:34:07 -0500

    Archivos del repositorio de Redes

---
+++ Ejercicios-Clase/RPC/PerlClient.pl
@@ -0,0 +1,11 @@
+use Frontier::Client;
+
+# Crear un objeto para representar el servidor XML-RPC
+$server_url = 'http://localhost:8000/RPC2';
+$server = Frontier::Client->new(url => $server_url);
+
+# Llamar al servidor remoto e imprimir los resultados
+$result = $server->call('add', 5, 3);
+$sum = $result;
+
+print "Sum: $sum\n";
