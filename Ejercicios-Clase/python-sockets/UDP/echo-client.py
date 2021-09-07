57666750f796ccda06aaadc81114f701ac83044d
NacxitQuetzalcoatl <86132894+NacxitCotuha@users.noreply.github.com>
2021-09-06 23:57:02 -0500

    Ejercicio UDP

--- Ejercicios-Clase/python-sockets/UDP/echo-client.py
+++ Ejercicios-Clase/python-sockets/UDP/echo-client.py
@@ -4,12 +4,12 @@ HOST = "127.0.0.1"  # El hostname o IP del servidor
 PORT = 54321  # El puerto usado por el servidor
 msgFromClient = "Hello UDP Server"
 bytesToSend = str.encode(msgFromClient)
-serverAddressPort = ("127.0.0.1", 54321)
+serverAddressPort: tuple[str, int] = (HOST, PORT)
 bufferSize = 1024
 
 # Crea un socket UDP del lado del cliente
 
-with socket.socket(socket.AF_INET,socket.SOCK_DGRAM) as UDPClientSocket:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as UDPClientSocket:
     # Enviando mensaje al servidor usando el socket UDP
     UDPClientSocket.sendto(bytesToSend, serverAddressPort)
     msgFromServer = UDPClientSocket.recvfrom(bufferSize)
